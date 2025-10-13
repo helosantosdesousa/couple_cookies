d893f59d5b95a0600cf4ee77387cefa68805e97f
helosantosdesousa <ssheloly@gmail.com>
2025-10-13 15:25:39 -0300

    fiz imgs

--- script/containers.js
+++
@@ -1,9 +0,0 @@
-  window.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('navbar');
-    const containerHome = document.getElementById('container_home');
-
-    if (navbar && containerHome) {
-      const navbarHeight = navbar.offsetHeight;
-      containerHome.style.paddingTop = navbarHeight + 'px';
-    }
-  });
